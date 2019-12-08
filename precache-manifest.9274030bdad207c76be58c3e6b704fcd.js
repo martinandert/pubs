592a70a5dba6596fe8ea42d52eaff8d88173d7ee
martinandert <mandert@gmail.com>
2019-12-08 19:10:42 +0100

    Updates

---
+++ precache-manifest.9274030bdad207c76be58c3e6b704fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cf69db08dff4c02b3b9cfac015f15e",
+    "url": "/pubs/index.html"
+  },
+  {
+    "revision": "780a771ba860336724e7",
+    "url": "/pubs/static/css/main.c2a8f7c3.chunk.css"
+  },
+  {
+    "revision": "54a7ef7e5e05bb690cab",
+    "url": "/pubs/static/js/2.65ffbd7a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pubs/static/js/2.65ffbd7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "780a771ba860336724e7",
+    "url": "/pubs/static/js/main.3f8b3334.chunk.js"
+  },
+  {
+    "revision": "2deabf53b630b3ffaac5",
+    "url": "/pubs/static/js/runtime-main.95915a43.js"
+  }
+]);
